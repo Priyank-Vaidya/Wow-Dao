55c3c48134fd28bfe199a93a46777e0c5b213d16
Abhinav Sharma <abhinav291sharma@gmail.com>
2023-09-15 23:28:26 +0530

    Modified index.html file's bootstrap cdn, modified navbar, made changes to layout of homepage

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,20 +3,30 @@ import React from 'react';
 export const NavBar = () => {
 
     return(
-      <>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="/">WowDao AI Project</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+        <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">ImaginAI</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+            <span class="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <a className="nav-item nav-link active" href="/">Home <span className="sr-only">(current)</span></a>
-              <a className="nav-item nav-link" href="/formpage">Submit Scan</a>
+          <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel" style={{width: '21em'}}>
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">ImaginAI</h5>
+              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/formpage">Submit Scan</a>
+                </li>
+              </ul>
             </div>
           </div>
-        </nav>
-      </>
+        </div>
+      </nav>
     )
 }
 
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -8,18 +8,13 @@ export const Home = () => {
         <>
             <NavBar />
             <br />
-            <div className='container'>
+            {/* <div className='container'>
                 <div className='row'>
                     <div className='col-lg-6'>
                         <blockquote className='blockquote'>
-                            <p className='mb-0'>A well-known quote, contained in a blockquote element.</p>
+                            <p className='mb-0' style={{paddingLeft: '4em'}}>ImaginAI</p>
                         </blockquote>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        </p>
+                        <p>A revolutionary tech aimed at improving the lives of those who can't afford modern healthcare!</p>
                     </div>
                     <div className='col-lg-8'>
                         <p>An example of the input and output:</p>
@@ -32,6 +27,31 @@ export const Home = () => {
                             <p className='mb-0 '>Made By:</p>
                             <footer className='blockquote-footer my-2'>Team <cite title='Source Title'>Byte Hogs</cite></footer>
                         </blockquote>
+                    </div>
+                </div>
+            </div> */}
+            <div className='container-fluid' style={{marginTop: '2em', paddingTop: '3em'}}>
+                <div class="container text-center">
+                    <div class="row">
+                        <h1><blockquote>ImaginAI</blockquote></h1>
+                    </div><br></br>
+                    <div class="row">
+                        <h2>We convert CT Scans - MRI Scans with help of Generative AI</h2>
+                    </div><br></br><br></br>
+
+                    <div class="row text-center">
+                        <div class="col">
+                            <h5>Input Image</h5>
+                            <img src={imginput} style={{marginTop:"20px", marginRight:"40px", marginLeft:"30px"}} className='img-fluid rounded' alt='...' />
+                        </div>
+                        <div class="col">
+                            <h5>Expected Image</h5>
+                            <img src={imgexpected} style={{marginTop:"20px"}} className='img-fluid rounded' alt='...' />
+                        </div>
+                        <div class="col">
+                            <h5>Output Image</h5>
+                            <img src={imgoutput} width={"260px"} style={{marginTop:"20px"}} className='img-fluid rounded' alt='...' />
+                        </div>
                     </div>
                 </div>
             </div>
