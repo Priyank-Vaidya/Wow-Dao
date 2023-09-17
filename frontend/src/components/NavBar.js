f5a27500f3a34b23b83160a3b257850f8eaceb35
Abhinav Sharma <abhinav291sharma@gmail.com>
2023-09-17 20:34:16 +0530

    Replaced class by className

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,10 +4,10 @@ import image from "./logo5.png"
 export const NavBar = () => {
 
     return(
-    <nav class="navbar bg-dark fixed-top">
-      <form class="container-fluid justify-content-start">
-        <a class="nav-brand nav-link" aria-current="page" href="/"><strong>ImaginAI</strong></a>
-        <button style={{marginLeft:'2%'}} class="btn btn-dark me-2" type="button"><a class="nav-link" aria-current="page" href="/formpage">Submit Scan</a></button>
+    <nav className="navbar bg-dark fixed-top">
+      <form className="container-fluid justify-content-start">
+        <a className="nav-brand nav-link" aria-current="page" href="/"><strong>ImaginAI</strong></a>
+        <button style={{marginLeft:'2%'}} className="btn btn-dark me-2" type="button"><a className="nav-link" aria-current="page" href="/formpage">Submit Scan</a></button>
       </form>
       <img style={{position:'absolute', marginLeft:'85%'}} width={'150px'} src={image} alt='none'></img>
     </nav>
--- frontend/src/pages/formpage.js
+++ frontend/src/pages/formpage.js
@@ -145,19 +145,19 @@ export const Formpage = () => {
         </div>
       </div> 
       
-      <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+      <div className="card" style="width: 18rem;">
+        <img src="..." className="card-img-top" alt="...">
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
         </div>
       </div>
       */}
         <NavBar />
         <div className="container-fluid d-flex justify-content-center align-items-center" style={{color:'white',minHeight: '100vh' , marginLeft: '10%vw', marginRight: '10%vw', justifyContent: 'space-around'}}>
-              <div class="row text-center" style={{marginTop:'4em',flexFlow: 'row', flex: 'row', flexShrink: 'inherit'}}>
-                    <div class="col col-lg-4 col-md-4 col-sm-4 col-xsm-4" style={{ borderColor: 'white', borderStyle: 'solid', borderWidth: '2px', paddingTop: '4px',borderRadius: '15px', width: "250px", height: "250px", marginRight: '30px'}}>
+              <div className="row text-center" style={{marginTop:'4em',flexFlow: 'row', flex: 'row', flexShrink: 'inherit'}}>
+                    <div className="col col-lg-4 col-md-4 col-sm-4 col-xsm-4" style={{ borderColor: 'white', borderStyle: 'solid', borderWidth: '2px', paddingTop: '4px',borderRadius: '15px', width: "250px", height: "250px", marginRight: '30px'}}>
                       <form onSubmit={handleSubmit}>
                         <h5><blockquote style={{ paddingTop: '4px'}}>Input CT Scan</blockquote></h5><br />
                         <div className="form-group">
@@ -181,7 +181,7 @@ export const Formpage = () => {
                         </button>
                       </form>
                     </div>
-                    <div class="col col-lg-4 col-md-4 col-sm-4 col-xsm-4" style={{ borderColor: 'white', borderStyle: 'solid', borderWidth: '2px',  paddingTop: '4px', borderRadius: '15px', width: "250px", height: "250px", marginLeft: '30px'}}>
+                    <div className="col col-lg-4 col-md-4 col-sm-4 col-xsm-4" style={{ borderColor: 'white', borderStyle: 'solid', borderWidth: '2px',  paddingTop: '4px', borderRadius: '15px', width: "250px", height: "250px", marginLeft: '30px'}}>
                       <h5><blockquote style={{ paddingTop: '4px'}}>Output MRI Scan</blockquote></h5><br />
                       <form>
                         {/* <img src={outputImage} className="rounded" style={{width: "100px", height: "100px", marginBottom: '42px' }} alt="Output" /><br /><br /> */}
